11c4d43b99e9c3d0079005d296b2b026a14f8c0c
Jerry <miracleswang@163.com>
2026-01-03 16:41:50 +0800

    struct结构体 treesort

--- ch05/function01/main.go
+++ ch05/function01/main.go
@@ -22,11 +22,12 @@ func hypot(x, y float64) float64 {
 }
 
 // func f(i int,j int,k int, s string,t string) {
-
-func f(i, j, k int, s, t string) {
+func f(i, j, k int, s, t string) { // 与上一行代码等价， 形参列表的参数和返回值列表中如果是相同类型，可以合并只写一次
 	fmt.Println(i, j, k, s, t)
 }
 
+
+// 
 func add(x int, y int) int {
 	return x + y
 }
